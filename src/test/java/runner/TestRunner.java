302eaf89b3de018a53d6a6b0332820a7aea398b5
rroshan200431@gmail.com <rroshan200431@gmail.com>
2026-02-05 15:14:16 +0530

    login test case

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,5 +1,15 @@
 package runner;
 
-public class TestRunner {
-
-}
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+@CucumberOptions(
+	    features = "src/test/resources/features",
+	    glue = {"stepdefinitions", "hooks"},
+	    plugin = {
+	        "pretty",
+	        "html:target/cucumber-report.html",
+	        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+	    }
+	)
+	public class TestRunner extends AbstractTestNGCucumberTests {
+	}
